86b0f6e662e5bc93a0edbebd4dd7ba0c73dd5219
Eliscolio <Eliscolino@gmail.com>
2022-03-09 21:08:45 -0600

    testsave

---
+++ week1/app.js
@@ -0,0 +1,20 @@
+class song{
+    constructor(title,artist,year){
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+    }
+    
+    describe(){
+    return `${this.title} by ${this.Artist} in {this.year}.`;
+    }
+}
+
+class music{
+    constructor(name){
+        this.title = title;
+        this.song = [];
+    }
+
+    addSong
+}
