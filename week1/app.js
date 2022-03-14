3fc8c23a610787c33201ade552c0287c84996acb
Eliscolio <Eliscolino@gmail.com>
2022-03-13 23:14:29 -0500

    week4

--- week1/app.js
+++ week1/app.js
@@ -1,20 +1,62 @@
-class song{
+class Song{
     constructor(title,artist,year){
         this.title = title;
         this.artist = artist;
         this.year = year;
     }
-    
+  
     describe(){
-    return `${this.title} by ${this.Artist} in {this.year}.`;
+    return `${this.title} by ${this.Artist} in ${this.year}`
     }
 }
 
-class music{
-    constructor(name){
-        this.title = title;
-        this.song = [];
+class Menu {
+    constructor() {
+        this.songs = [];
     }
+    
+    mainMenu(){
+        let menuSelection =prompt(`
+            1)Create a song
+            2)Play a song
+            3)Delete a song
+        `)
+        switch (menuSelection){
+            case "1":
+                this.addSong();
+                break;
+            case "2":
+                this.viewSongs();
+                break;
+            case "3":
+                this.deleteSong()
+                break;
+            default:
+                this.mainMenu();
+        }
+    }
+   addSong() { 
+        const newSong = new Song(
+            prompt("Enter Title"),
+            prompt("enter artist"),
+            prompt("enter year")
+        );
+        this.Songs.push(newSong);
+        this.mainMenu();
+    }
+    deleteSong(){
+        alert("Delete a book")
+    } 
+    viewSongs() {
+        let listOfSongs = "";
+        for (let song of this.songs ){
+            listOfSongs += (Song.describe() + "\n");
+        }
+        prompt(listOfSongs + "\n\nEnter 0 to back to the main menu: ");
+        this.mainMenu();
 
-    addSong
+    }
 }
+const menu = new Menu();
+menu.mainMenu();
+console.log(Song)
